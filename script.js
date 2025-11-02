3d90c4d9fc6dc207574564a9ff9c6b09301becdf
tawhidwebdev <tawhidlimon.dev@gmail.com>
2025-11-02 23:55:48 +0600

    News by Categories

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+const newsContainer = document.getElementById('news-container');
+
 const categoryData = async () => {
   try {
     const res = await fetch('https://news-api-fs.vercel.app/api/categories');
     const data = await res.json();
     loadCategories(data.categories);
+    
   } catch (error) {
     console.error('Error fetching data:', error);
   }
@@ -17,13 +20,45 @@ const loadCategories = (categories) => {
 
   categories.forEach(category => {
     const li = document.createElement('li');
-    li.className = 'hover:decoration-red-500 hover:underline decoration-[4px] underline-offset-12 transition-colors duration-200';
-    li.innerHTML = `<a href="#">${category.title}</a>`;
+    li.className = 'hover:decoration-red-600 hover:underline decoration-[4px] underline-offset-20 transition-colors duration-200';
+    li.innerHTML = `<a id="${category.id}" href="#">${category.title}</a>`;
     ul.appendChild(li);
   });
+  categoryList.addEventListener('click', (e) => {
+    const allLinks = document.querySelectorAll('#category-list a');
+    allLinks.forEach(link => {
+      link.classList.remove('underline')
+    })
+    
+    if (e.target.nodeName === 'A') {
+      e.target.className = 'underline decoration-red-600 decoration-[4px] underline-offset-20 transition-colors duration-200';
 
+      const categoryId = e.target.getAttribute('id');
+      categoryNewsId(categoryId);
+    }
+
+  })
   categoryList.appendChild(ul);
 };
 
+const categoryNewsId = (categoryId) => {
+  fetch(`https://news-api-fs.vercel.app/api/categories/${categoryId}`)
+  .then(res => res.json())
+  .then(data => {
+    showCategoryNews(data.articles);
+  })
+  .catch(err => {
+    console.error('Error fetching category data:', err);
+  })
+}
+
+const showCategoryNews = (allNews) => {
+  newsContainer.innerHTML = '';
+  allNews.forEach(news => {
+    newsContainer.innerHTML += `<h1>${news.title}</h1>`
+  })
+}
+
 // call the function to load data
 categoryData();
+
